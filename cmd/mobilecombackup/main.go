bb2f29a0f8465e13e3a8fc4022b7d39a0c3b04e5
Phillip Green II <phillip.green.ii@gmail.com>
2022-02-24 01:07:08 +0000

    add arguments

--- cmd/mobilecombackup/main.go
+++ cmd/mobilecombackup/main.go
@@ -1,22 +1,37 @@
 package main
 
 import (
+	"flag"
 	"fmt"
-  "os"
-  "github.com/phillipgreen/mobilecombackup/pkg/mobilecombackup"
+	"os"
+
+	"github.com/phillipgreen/mobilecombackup/pkg/mobilecombackup"
 )
 
 func main() {
-  exitCode := 0
-  defer func() { os.Exit(exitCode) }()
-  
-  mcb := mobilecombackup.Init("./testdata/archive")
+	exitCode := 0
+	defer func() { os.Exit(exitCode) }()
 
-  result,err := mcb.Process("./testdata/to_process")
-  if err != nil {
-    fmt.Fprintf(os.Stderr, "Failure: %v\n", err.Error())
-    exitCode = 1
-  } else {
-    fmt.Printf("Success: %v\n",result)
+	repoPath := flag.String("repo", ".", "path which contains repository")
+
+	flag.Parse()
+	pathsToProcess := flag.Args()
+
+  if len(pathsToProcess) <= 0 {
+    	fmt.Fprintf(os.Stderr, "Atleast one path to process must be specified\n")
+			exitCode = 1
+    return
   }
+
+	mcb := mobilecombackup.Init(*repoPath)
+
+	for _, path := range pathsToProcess {
+		result, err := mcb.Process(path)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Failure: %v\n", err.Error())
+			exitCode = 1
+		} else {
+			fmt.Printf("Success: %v\n", result)
+		}
+	}
 }
