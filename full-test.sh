587e79edfb782e896652462345b7941a89835af9
Phillip Green II <phillip.green.ii@gmail.com>
2025-08-14 09:20:41 -0400

    add prefix to messages

--- full-test.sh
+++ full-test.sh
@@ -2,65 +2,65 @@
 
 set -e
 
-echo "Build Project"
+echo ">> Build Project"
 devbox run build-cli
 
-echo "Create Repo Directory"
+echo ">> Create Repo Directory"
 repodir=./tmp/full-test
 rm -rf "$repodir"
 mkdir -p $repodir
 
-echo "Initialize Repository"
+echo ">> Initialize Repository"
 ./mobilecombackup init --repo-root="$repodir"
 
-echo "Validate Fresh Repository"
+echo ">> Validate Fresh Repository"
 ./mobilecombackup validate --repo-root="$repodir"
 
-echo "Info should work with empty repository"
+echo ">> Info should work with empty repository"
 ./mobilecombackup info --repo-root="$repodir"
 
-echo "Setting adding contacts.yaml"
+echo ">> Setting adding contacts.yaml"
 cat > "$repodir/contacts.yaml" << EOF
 contacts: 
-    - name: "Oscar Wild"
+    - name: "Oscar Wilde"
       numbers:
         - "5555550004"
 EOF
 ./mobilecombackup reprocess-contacts --repo-root="$repodir" 
 
-echo "Validate After contact change"
+echo ">> Validate After contact change"
 ./mobilecombackup validate --repo-root="$repodir" 
 
-echo "Import Test Data"
+echo ">> Import Test Data"
 ./mobilecombackup import --repo-root="$repodir" testdata
 
-echo "Validate Repository after Successful Import"
+echo ">> Validate Repository after Successful Import"
 ./mobilecombackup validate --repo-root="$repodir"
 
-echo "Should have imported calls"
+echo ">> Should have imported calls"
 if [[ $(ls -1 "$repodir/calls" | wc -l) -eq 0 ]]; then
 	echo "ERROR: No calls found" >&2
 	exit 2
 fi
 
-echo "Should have imported smses"
+echo ">> Should have imported smses"
 if [[ $(ls -1 "$repodir/sms" | wc -l) -eq 0 ]]; then
 	echo "ERROR: No smses found" >&2
 	exit 2
 fi
 
-echo "Should have imported attachments"
+echo ">> Should have imported attachments"
 if [[ $(ls -1 "$repodir/attachments" | wc -l) -eq 0 ]]; then
 	echo "ERROR: No attachments found" >&2
 	exit 2
 fi
 
-echo "Info should work with imported data"
+echo ">> Info should work with imported data"
 ./mobilecombackup info --repo-root="$repodir"
 
-echo "Re-Import Test Data should work"
+echo ">> Re-Import Test Data should work"
 ./mobilecombackup import --repo-root="$repodir" testdata
 
-echo "Validate Repository after Re-Import should work"
+echo ">> Validate Repository after Re-Import should work"
 ./mobilecombackup validate --repo-root="$repodir"
 
